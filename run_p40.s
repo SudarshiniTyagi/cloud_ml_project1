41717247352e35e821ed7f8365a280ded47ecb52
SudarshiniTyagi <tyagi.sudarshini@gmail.com>
2020-03-22 23:27:32 -0400

    Minor changes

--- run_p40.s
+++ run_p40.s
@@ -12,4 +12,4 @@
 
 module load  python3/intel/3.6.3 cuda/9.0.176  nccl/cuda9.0/2.4.2
 
-~/pytorch_env/py3.6.3/bin/python examples/imagenet/main.py -a resnet18 -b 128 --lr 0.01 --epochs 1 /scratch/st3688/imagenet_small_subset/
+~/pytorch_env/py3.6.3/bin/python imagenet/main.py -a resnet18 -b 128 --lr 0.01 --epochs 1 /scratch/st3688/imagenet_small_subset/
